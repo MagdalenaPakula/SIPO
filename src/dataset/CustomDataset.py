36919c2760e010885d15b0f50ed715856f51c1b9
MagdalenaPakula <magdalena.pakula001@gmail.com>
2024-06-03 21:35:36 +0200

    - Added 2 tokenizers (BERT and NLTK) - to test - Added BERT impl - to test

--- src/dataset/CustomDataset.py
+++ src/dataset/CustomDataset.py
@@ -1,18 +1,27 @@
 import sys
 import os
+from torch.utils.data import Dataset
+import pandas as pd
+
+from src.tools import load
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from tools import load
 
-from torch.utils.data import Dataset
 
 class CustomDataset(Dataset):
-    def __init__(self, data=None, targets=None, data_path=None):
+    def __init__(self, data=None, targets=None, data_path=None, tokenizer=None):
         if data_path:
             self.data, self.targets = load(data_path)
         else:
             self.data = data
             self.targets = targets
+        self.tokenizer = tokenizer
+
+    def load(self, data_path):
+        df = pd.read_csv(data_path)
+        data = df['review'].tolist()
+        targets = df['sentiment'].tolist()
+        return data, targets
 
     def __len__(self):
         return len(self.data)
@@ -20,4 +29,9 @@ class CustomDataset(Dataset):
     def __getitem__(self, index):
         sample = self.data[index]
         label = self.targets[index]
+        if self.tokenizer:
+            sample = self.tokenizer.tokenize(sample)
         return sample, label
+
+
+
