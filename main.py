d661c4a194e4a9ffdb1a9cd215a72e3e770434c0
MagdalenaPakula <magdalena.pakula001@gmail.com>
2024-06-05 21:20:53 +0200

    added information printing + ability to save the model

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import joblib
+from tqdm import tqdm
+
 from src.dataset.CustomDataset import CustomDataset
 from src.dataset.extraction import TF_IDF, BoW, WordEmbeddings
 from src.dataset.classification import Classification
@@ -6,17 +9,25 @@ from sklearn.metrics import classification_report
 
 
 if __name__ == '__main__':
-    dataset = CustomDataset(data_path="data\processed\Translated_IMDB_Dataset_2_half.csv")
+    print("Loading the data...")
+    dataset = CustomDataset(data_path="data/processed/Translated_IMDB_Dataset_PROCESSED.csv")
 
     X = TF_IDF(dataset.data)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, dataset.targets, test_size=0.2)
+    X_train, X_test, y_train, y_test = train_test_split(X, dataset.targets, test_size=0.5)
 
     train_dataset = CustomDataset(data=X_train, targets=y_train)
     test_dataset = CustomDataset(data=X_test, targets=y_test)
 
     clf = Classification()
+    print("Training the model...")
     clf.SVM_train(train_dataset)
 
+    print("Testing the model...")
     result = clf.test(test_dataset.data)
     print(classification_report(test_dataset.targets, result))
+
+    # Save the trained model
+    print("Saving the model...")
+    joblib.dump(clf, 'src/models/model_TF_SVM.joblib')
+    print("Model saved as: model_TF_SVM.joblib")
